598c5071d41da55ac57002f8657f0e281034be50
themoat <aditya@ayemalls.com>
2022-05-08 22:08:50 -0700

    Update marketplace.sol

--- marketplace.sol
+++ marketplace.sol
@@ -1,5 +1,85 @@
 pragma solidity >=0.4.25 <0.6.0;
-contract SimplMarketPlace{
+contract SimpleMarketPlace{
+
+  enum StateType{
+  ItemAvailable,
+  OfferPlaced,
+  Accepted
+  }
+
+  address public InstanceOwner;
+  string Description;
+  int public AskingPrice;
+  StateType public State;
+  
+  address public InstanceBuyer;
+  int public OfferPrice;
+  
+
+
+  constructor(string memory description, int price) public {
+        InstanceOwner = msg.sender;
+        AskingPrice = price;
+        Description = description;
+        State = StateType.ItemAvailable;
+  
+  }
+  
+  function MakeOffer(int offerPrice) public{
+  
+    if (offerPrice == 0) {
+      revert();
+     }
+     
+    if State != StateType.ItemAvailable{
+      revert();
+     }
+     
+    if (InstanceOwner == msg.sender){
+      revert();
+      }
+      
+    InstanceBuyer = msg.sender;
+    OfferPrice = offerprice;;
+    State = StateType.OfferPlaced;
+    
+    
+ function Reject() public{
+ 
+    if (State!= StateType.OfferPlaced){
+      revert();
+    }
+    
+    if InstanceOwner!= msg.sender{
+      revert();
+    }
+    
+    InstanceBuyer = 0x0000000000000000000000000000000000000000;
+    State = StateType.ItemAvailable;
+    }
+    
+    
+ function Accept() public{
+ 
+    if msg.sender!= InstanceOwner{
+      revert();
+    }
+    
+    State = StateType.Accepted;
+    
+    }
+    
+}
+
+
+
+    
+    
+      
+     
+      
+      
+
 
 
 
